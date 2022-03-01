73463360e06e84e93ec383d4276c6684b9bb692d
avidbiscuit <50924190+avidbiscuit@users.noreply.github.com>
2022-03-01 10:11:49 +0000

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -1,4 +1,16 @@
-print("Hello")
-print(10-5-6+4)
-age = 10-5
-print(age)
+#double multiplication signs raise numerics to the power
+from email.mime import base
+from functools import total_ordering
+
+
+print(2**3)
+#expected output:8
+print((7+5)*3)
+#CTRL + F5 to run file
+
+base_pay = 15
+tips = float(input("Please input tips earned: "))
+format_float = "{:.2f}".format(tips)
+hours_worked = int(input("Please input your hours worked: "))
+print("€", (base_pay *hours_worked) + format_float)
+print("Checking to see if github desktop is working!")
