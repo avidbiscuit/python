630805a0082fca75aa7e2acc0fd9997606c1d0f3
avidbiscuit <50924190+avidbiscuit@users.noreply.github.com>
2022-03-01 11:53:15 +0000

    Adding functions to practical 4.py
    -Added functions
    -Created Loops and Lists
    -Used a given string and breaks it into a new list

---
+++ practical 4-if loops.py
@@ -0,0 +1,20 @@
+#Task 1.1 Write a loop to print out all the numbers between 0 and 100
+#for number in range(0,101,1):
+    #print(number)
+#Task 1.2 Use a loop to print out all the even numbers between 0 and 100
+from ast import Break
+
+
+def task12():
+    class_list = ["Alan", "Julie", "Kevin", "Patrick", "Mark", "Paula", "Shauna", "Sinead", "Tomas"]
+    for x in class_list:
+        print("hello ",x, " how are you!")
+    #Task 1.4 Given a string variable please print each letter of the string
+        
+def task22():
+    given_string = "DKIT Business Studies"
+    for x in given_string:
+        new_list = []
+        new_list.append(x)
+        print(new_list)       
+task22()
