2ae07b2ae714e8170663bb3fc1a5d9046a882343
avidbiscuit <50924190+avidbiscuit@users.noreply.github.com>
2022-03-01 10:57:45 +0000

    working through exercises till 11am
    Adding exercises and working through them

--- exercises.py
+++ exercises.py
@@ -1,6 +1,7 @@
 #double multiplication signs raise numerics to the power
-from email.mime import base
-from functools import total_ordering
+
+from itertools import count
+from random import choice
 
 
 print(2**3)
@@ -9,9 +10,21 @@ print((7+5)*3)
 #CTRL + F5 to run file
 
 base_pay = 15
-tips = float(input("Please input tips earned: "))
+tips = 40
 format_float = "{:.2f}".format(tips)
-hours_worked = int(input("Please input your hours worked: "))
-print("€", (base_pay *hours_worked) + format_float)
+hours_worked = 40
+
 print("Checking to see if github desktop is working!")
 print("double checking!")
+
+
+#1.1 Declare a varaible with your first and last name, use indexing to get your initials
+first_name = "sean"
+last_name = "magennis"
+print(first_name[0].capitalize(),last_name[0].capitalize())
+#1.2 Declare a variable with a country name, use indexing to get the first 3 letters of the country
+choice = "ireland"
+print(choice[0:3])
+#1.3 Declare a string variable to hold the name of a country. Use indexing to print the last three letters of the country
+country = "germany"
+print(country[-3:])
